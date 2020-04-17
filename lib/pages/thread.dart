01750d5b9a96dfa0b977fb3219d40c9d5cad40fd
Pradip-Dhanraj <32770009+Pradip-Dhanraj@users.noreply.github.com>
2020-04-17 20:01:39 +0530

    updated layout with child widget

--- lib/pages/thread.dart
+++ lib/pages/thread.dart
@@ -32,15 +32,50 @@ class _ThreadState extends State<Thread> {
         color: blueshade02,
         child: Column(
           children: <Widget>[
-            getContainerLayout(
-              child: Container(),
+            Container(
               height: (appheight / 3) * 2,
-              backgroundcolor: blueshade02,
-              sideBarColor: s2SidebarColor,
-              sidebarTextColor: s3SidebarTextColor,
+              child: Stack(
+                children: <Widget>[
+                  FadeInImage.memoryNetwork(
+                    placeholder: kTransparentImage,
+                    image:
+                        "https://i.pinimg.com/736x/cb/f4/78/cbf4781a70862ffb8c43ce1d4b991620.jpg",
+                    fit: BoxFit.fill,
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Stack(
+                        children: <Widget>[
+                          getContainerLayout(
+                            child: Container(),
+                            height: (appheight / 3),
+                            backgroundcolor: blueshade02,
+                            sideBarColor: s2SidebarColor,
+                            sidebarTextColor: s3SidebarTextColor,
+                            sidebarText: "C. Ronaldo",
+                          ),
+                          getNavbar(),
+                        ],
+                      ),
+                      getContainerLayout(
+                          child: Container(),
+                          height: (appheight / 3),
+                          backgroundcolor: blueshade02,
+                          sideBarColor: s2SidebarColor,
+                          sidebarTextColor: s3SidebarTextColor,
+                          sidebarText: "highlights",
+                          sidebarIcon: Icon(
+                            Icons.data_usage,
+                            color: Colors.white54,
+                          ),
+                          opacity: .9),
+                    ],
+                  ),
+                ],
+              ),
             ),
             getContainerLayout(
-              child: Container(),
+              child: getStatisticsLayout(),
               height: appheight / 3,
               backgroundcolor: s3background,
               sideBarColor: s3SidebarColor,
@@ -49,6 +84,7 @@ class _ThreadState extends State<Thread> {
                 Icons.data_usage,
                 color: Colors.white54,
               ),
+              sidebarText: "Statistics",
             ),
           ],
         ),
@@ -57,79 +93,66 @@ class _ThreadState extends State<Thread> {
   }
 }
 
-/*
-child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              getContainerLayout(
-                child: Container(),
-                height: (appheight * .3) * 2,
-                backgroundcolor: blueshade02,
-                sideBarColor: s2SidebarColor,
-                sidebarTextColor: s3SidebarTextColor,
-                sidebarIcon: Icon(
-                  Icons.data_usage,
-                  color: Colors.white54,
-                ),
-              ),
-              getContainerLayout(
-                child: Container(),
-                height: appheight * .3,
-                backgroundcolor: s3background,
-                sideBarColor: s3SidebarColor,
-                sidebarTextColor: s3SidebarTextColor,
-                sidebarIcon: Icon(
-                  Icons.data_usage,
-                  color: Colors.white54,
-                ),
-              ),
-            ],
-          ),
-*/
+Widget getStatisticsLayout() {
+  var lst = [
+    "one",
+    "two",
+    "three",
+    "four",
+  ].toList();
+  return Container(
+    child: ListView.builder(
+      itemCount: lst.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Row(
+          children: <Widget>[
+            Text("${lst[index]}"),
+          ],
+        );
+      },
+    ),
+  );
+}
 
 Widget getContainerLayout({
   @required Widget child,
   @required double height,
-  String backgroundimageurl,
   Color backgroundcolor = blueshade01,
   Color sidebarTextColor = Colors.white54,
   Color textColor = Colors.white54,
   Icon sidebarIcon,
-  @required sideBarColor,
+  double opacity = .5,
+  @required Color sideBarColor,
+  String sidebarText = "",
 }) {
   return Container(
+    color: backgroundcolor.withOpacity(opacity),
     height: height,
-    child: Stack(
+    child: Row(
       children: <Widget>[
-        (backgroundimageurl != null)
-            ? FadeInImage.memoryNetwork(
-                placeholder: kTransparentImage,
-                image: backgroundimageurl,
-                fit: BoxFit.fill,
-              )
-            : Container(
-                color: backgroundcolor,
-              ),
         Container(
-          color: sideBarColor,
-          height: double.infinity,
-          padding: EdgeInsets.all(20),
+          padding: EdgeInsets.symmetric(vertical: 20),
           width: 70,
+          height: height,
           child: RotatedBox(
-              quarterTurns: -1,
-              child: Row(
-                children: <Widget>[
-                  Text(
-                    "sample".toUpperCase(),
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: sidebarTextColor,
-                    ),
+            quarterTurns: -1,
+            child: Row(
+              children: <Widget>[
+                Text(
+                  sidebarText.toUpperCase(),
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: sidebarTextColor,
                   ),
-                  Spacer(),
-                  sidebarIcon == null ? Container() : sidebarIcon,
-                ],
-              )),
+                ),
+                Spacer(),
+                sidebarIcon == null ? Container() : sidebarIcon,
+              ],
+            ),
+          ),
+        ),
+        Container(
+          child: Expanded(child: child),
         ),
       ],
     ),
@@ -137,17 +160,25 @@ Widget getContainerLayout({
 }
 
 Widget getNavbar() {
-  return Row(
-    children: <Widget>[
-      Icon(
-        Icons.data_usage,
-        color: textcolor,
-      ),
-      Spacer(),
-      Icon(
-        Icons.donut_small,
-        color: textcolor,
-      ),
-    ],
+  return SafeArea(
+    child: Row(
+      children: <Widget>[
+        Container(
+          width: 70,
+          child: Icon(
+            Icons.data_usage,
+            color: textcolor,
+          ),
+        ),
+        Spacer(),
+        Container(
+          width: 70,
+          child: Icon(
+            Icons.menu,
+            color: textcolor,
+          ),
+        ),
+      ],
+    ),
   );
 }
